f126680ca68f8d3863bdd79f9149d4e7331d2b51
basha105 <brentbasha17@gmail.com>
2024-08-02 15:41:41 -0700

    Trying to figure out creating elements using js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log("HEY!");
+import pageLoad from './page_load';
+
+const contentContainer = document.getElementById("content");
+const newTitle = pageLoad;
+contentContainer.appendChild(newTitle);
+
